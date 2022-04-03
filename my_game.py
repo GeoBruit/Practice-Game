ba6fbb81bb54e46a889296e913d6cc26316e4a0a
GeoBruit <george_cioboata@yahoo.com>
2022-04-03 17:46:13 +0100

    Added one more class for the  player to choose from and new attacks

--- my_game.py
+++ my_game.py
@@ -21,16 +21,16 @@ class Character:
                     damage_took = damage - self.shield
                     self.life -= damage_took
                     self.shield = 0
-                    print(f"Shield down to {self.shield}, life is now {self.life}")
+                    print(f"{self.name} Shield down to {self.shield}, life is now {self.life}")
                 elif self.shield != 0:
                     self.shield -= damage
                     print(f"Shield down to {self.shield}")
             elif self.shield == 0:
                 self.life -= damage
-                print(f"Shield down to {self.shield}, life is now {self.life}")
+                print(f"{self.name} Shield down to {self.shield}, life is now {self.life}")
 
         else:
-            print("The opponent is dead you won!!!")
+            print(f"{self.name} is dead you won!!!")
             self.life = 0
 
     def eat_spam(self):
@@ -54,8 +54,8 @@ class Warrior(Character):
         self.life = life
         self.used = False
 
-
-    def sword_swing(self, enemy:Character):
+    @staticmethod
+    def sword_swing(enemy: Character):
         """
         Function that allow us to attack  the enemy
         :param enemy: The enemy we are attacking
@@ -64,13 +64,15 @@ class Warrior(Character):
         enemy.take_damage(power)
         print(f"You attacked with the sward swing ability, {enemy.name}")
 
-    def spam_trow(self, enemy:Character):
+    @staticmethod
+    def spam_trow(enemy:Character):
         """An attack function, you trow a piece of old spam at the enemy and give him damage"""
         power = 25
         enemy.take_damage(power)
-        print(f"Spam attack succesfull enemy life: {enemy.life}")
+        print(f"Spam attack successful enemy life: {enemy.life}")
 
-    def kill_them_al(self, enemy):
+
+    def kill_them_all(self, enemy):
         """Most powerfull attack function, when used it inflicts the most damage for the Warrior class
         Can only be used once and only when playes' life is lower then 50"""
         if not self.used:
@@ -95,13 +97,17 @@ class Wizard(Character):
         self.name = mana
         self.used = False
 
-    def magic_spam(self, enemy:Character):
+    @staticmethod
+    def magic_spam(enemy: Character):
         damage = random.randint(30, 45)
         enemy.take_damage(damage)
+        print(f"The magic spam worked. Enemy life: {enemy.life}")
 
-    def wand_of_terror(self, enemy:Character):
+    @staticmethod
+    def wand_of_terror(enemy:Character):
         damage = random.randint(40, 60)
         enemy.take_damage(damage)
+        print(f"The want of terror attacked enemy life: {enemy.life}")
 
     def thunder_rain(self, enemy:Character):
         if not self.used:
@@ -109,10 +115,62 @@ class Wizard(Character):
                 damage = random.randint(60, 90)
                 enemy.take_damage(damage)
                 self.used = True
-                print(f"{enemy.name} attacked succesffuly damage done {damage}, {enemy.name} life is {enemy.life}")
+                print(f"{enemy.name} attacked successful damage done {damage}, {enemy.name} life is {enemy.life}")
             else:
                 print("You can t use this ability yet...")
         else:
             print("You can only used this ability once...already used...try spam attack")
 
 
+
+def choose_character():
+    character_choose = input("Please choose 1 to play as a Warrior or 2 to play as a Wizard: ")
+
+    global choose_name
+
+    choose_name = input("Name your character: ")
+
+    global player
+
+    if character_choose == "1":
+        player = Warrior(choose_name)
+        return player
+    else:
+        player = Wizard(choose_name)
+        return player
+
+
+def choose_opponent():
+
+    global opponent
+
+    if player == Warrior:
+        opponent = Wizard("Slayer")
+        return opponent
+    else:
+        opponent = Warrior("Slayer")
+        return opponent
+
+
+def choose_attack():
+    if isinstance(player, Warrior):
+        attack = input("Choose your attack move: 1.Sword Swing, 2.Spam Trow, 3.Kill Them All: ")
+        if attack == "1":
+            player.sword_swing(opponent)
+        elif attack == "2":
+            player.spam_trow(opponent)
+        elif attack == "3":
+            player.kill_them_all(opponent)
+    elif isinstance(player, Wizard):
+        attack = input("Choose your attack move: 1.Magic Spam, 2.Wand Of Terror, 3.Thunder Rain: ")
+        if attack == "1":
+            player.magic_spam(opponent)
+        elif attack == "2":
+            player.wand_of_terror(opponent)
+        elif attack == "3":
+            player.thunder_rain(opponent)
+
+
+
+def main():
+    pass
